9de0881e75fa0810634b228adf08cf355e415ecc
Christopher White <christopher.white@slicelife.com>
2022-08-30 21:19:28 +0100

    Initial commit

---
+++ ImageEraser/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ImageEraser
+//
+//  Created by Christopher White on 30/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageEraser/ImageEraserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEraserApp.swift
+//  ImageEraser
+//
+//  Created by Christopher White on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageEraserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
