399c110b4c64c9990256c5fcb7d8975ba1a50779
Christopher White <christopher.white@slicelife.com>
2022-08-30 21:38:04 +0100

    Added button

--- ImageEraser/ContentView.swift
+++ ImageEraser/ContentView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
+    let button = RiveViewModel(fileName: "button", autoPlay: false)
     var body: some View {
         VStack {
+            button.view()
+                .frame(width: 236, height: 64)
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .overlay(
+                    Label("Select a photo", systemImage: "photo.fill")
+                        .offset(x: 4, y: 4)
+                )
+                .onTapGesture {
+                    try? button.play(animationName: "active")
+                }
         }
     }
 }
