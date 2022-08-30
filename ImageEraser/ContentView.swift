4789756b8b3b9496910197cce1583f937fec11f3
Christopher White <christopher.white@slicelife.com>
2022-08-30 22:00:19 +0100

    Added photo picker

--- ImageEraser/ContentView.swift
+++ ImageEraser/ContentView.swift
@@ -7,46 +7,62 @@
 
 import SwiftUI
 import RiveRuntime
+import PhotosUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem? = nil
+
+    var body: some View {
+        VStack {
+            Text("erase objects from images.")
+                .frame(width: 260, alignment: .leading)
+                .font(.largeTitle)
+                .bold()
+            PhotosPicker(
+                selection: $selectedItem,
+                matching: .images,
+                photoLibrary: .shared()
+            ) {
+                SelectContentView()
+            }
+        }
+        .tint(.black)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            RiveViewModel(fileName: "shapes").view()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .ignoresSafeArea()
+                .blur(radius: 30)
+                .blendMode(.hardLight)
+        )
+        .background(
+            Image("Spline")
+                .blur(radius: 50)
+                .offset(x: 200, y: 100)
+        )
+    }
+}
+
+struct SelectContentView: View {
     let button = RiveViewModel(fileName: "button", autoPlay: false)
+
     var body: some View {
         VStack(alignment: .leading) {
-                Text("erase objects from images.")
-                    .frame(width: 260, alignment: .leading)
-                    .font(.largeTitle)
-                    .bold()
-                button.view()
-                    .frame(width: 236, height: 64)
-                    .background(
-                        Color.black
-                            .cornerRadius(30)
-                            .blur(radius: 10)
-                            .opacity(0.3)
-                            .offset(y: 10)
-                    )
-                    .overlay(
-                        Label("Select a photo", systemImage: "photo.fill")
-                            .bold()
-                            .offset(x: 4, y: 4)
-                    )
-                    .onTapGesture {
-                        try? button.play(animationName: "active")
-                    }
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                RiveViewModel(fileName: "shapes").view()
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .ignoresSafeArea()
-                    .blur(radius: 30)
-                    .blendMode(.hardLight)
-            )
-            .background(
-                Image("Spline")
-                    .blur(radius: 50)
-                    .offset(x: 200, y: 100)
-            )
+            button.view()
+                .frame(width: 236, height: 64)
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .overlay(
+                    Label("Select a photo", systemImage: "photo.fill")
+                        .bold()
+                        .offset(x: 4, y: 4)
+                )
+        }
     }
 }
 
