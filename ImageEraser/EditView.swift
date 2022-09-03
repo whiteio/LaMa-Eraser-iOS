656ab1a8e46c0f61fa31619212ee643a6b78ace1
Christopher White <christopher.white@slicelife.com>
2022-09-03 13:50:39 +0100

    Removed extra navbar sizing stuff:

--- ImageEraser/EditView.swift
+++ ImageEraser/EditView.swift
@@ -23,45 +23,41 @@ struct EditView: View {
 
     @State var scale: CGFloat = 1.0
     @State var offset: Coordinates = Coordinates(x: 0, y: 0)
-    @State var imageTopPadding: CGFloat = 0.0
 
     var body: some View {
-        VStack {
-            ZoomableScrollView {
-                Rectangle()
-                    .padding(.top, imageTopPadding)
-                    .background(NavBarAccessor { navBar in
-                        imageTopPadding = navBar.frame.height
+        GeometryReader { geometry in
+            VStack {
+                ZoomableScrollView {
+                    Rectangle()
+                }
+            }
+            .navigationTitle("ERASE")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.uturn.backward.circle")
                     })
+                    .tint(.white)
+                    .disabled(undoDisabled)
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.uturn.forward.circle")
+                    })
+                    .tint(.white)
+                    .disabled(redoDisabled)
+                }
             }
-        }
-        .ignoresSafeArea()
-        .navigationTitle("ERASE")
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarBackButtonHidden()
-        .toolbar {
-            ToolbarItemGroup(placement: .navigationBarLeading) {
-                Button(action: {}, label: {
-                    Image(systemName: "arrow.uturn.backward.circle")
-                })
-                .tint(.white)
-                .disabled(undoDisabled)
-                Button(action: {}, label: {
-                    Image(systemName: "arrow.uturn.forward.circle")
-                })
-                .tint(.white)
-                .disabled(redoDisabled)
-            }
-        }
-        .toolbar {
-            ToolbarItemGroup(placement: .bottomBar) {
-                Button(action: {}, label: {
-                    Text("Cancel")
-                })
-                Spacer()
-                Button(action: {}, label: {
-                    Text("Save")
-                })
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button(action: {}, label: {
+                        Text("Cancel")
+                    })
+                    Spacer()
+                    Button(action: {}, label: {
+                        Text("Save")
+                    })
+                }
             }
         }
     }
@@ -72,33 +68,6 @@ struct EditView_Previews: PreviewProvider {
         NavigationView {
             EditView(photoData: Data())
                 .preferredColorScheme(.dark)
-        }
-    }
-}
-
-struct NavBarAccessor: UIViewControllerRepresentable {
-    var callback: (UINavigationBar) -> Void
-    private let proxyController = ViewController()
-
-    func makeUIViewController(context: UIViewControllerRepresentableContext<NavBarAccessor>) ->
-    UIViewController {
-        proxyController.callback = callback
-        return proxyController
-    }
-
-    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<NavBarAccessor>) {
-    }
-
-    typealias UIViewControllerType = UIViewController
-
-    private class ViewController: UIViewController {
-        var callback: (UINavigationBar) -> Void = { _ in }
-
-        override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            if let navBar = self.navigationController {
-                self.callback(navBar.navigationBar)
-            }
         }
     }
 }
