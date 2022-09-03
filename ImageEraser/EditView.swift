c46f7ba7a4fa5f7cf69362ef0fd743986875eeb0
Christopher White <christopher.white@slicelife.com>
2022-09-03 11:30:07 +0100

    Added EditView which will show the selected image

---
+++ ImageEraser/EditView.swift
@@ -0,0 +1,53 @@
+//
+//  EditView.swift
+//  ImageEraser
+//
+//  Created by Christopher White on 02/09/2022.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @State var undoDisabled = false
+    @State var redoDisabled = true
+
+    var body: some View {
+        Rectangle()
+            .navigationTitle("ERASE")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.uturn.backward.circle")
+                    })
+                    .tint(.white)
+                    .disabled(undoDisabled)
+                    Button(action: {}, label: {
+                        Image(systemName: "arrow.uturn.forward.circle")
+                    })
+                    .tint(.white)
+                    .disabled(redoDisabled)
+                }
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button(action: {}, label: {
+                        Text("Cancel")
+                    })
+                    Spacer()
+                    Button(action: {}, label: {
+                        Text("Save")
+                    })
+                }
+            }
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            EditView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
