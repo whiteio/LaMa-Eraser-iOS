a41478afe1e327195a61471e96ca6bc43d0927cf
Christopher White <christopher.white@slicelife.com>
2022-09-04 11:03:54 +0100

    Created array group in extensions group to hold type specific array extensions

---
+++ ImageEraser/Extensions/Array/PointsSegment_Array+Extras.swift
@@ -0,0 +1,37 @@
+//
+//  PointsSegment_Array+Extras.swift
+//  ImageEraser
+//
+//  Created by Christopher White on 04/09/2022.
+//
+
+import SwiftUI
+
+extension Array where Element == PointsSegment {
+    func scaledSegmentsToPath() -> CGPath {
+        var path = Path()
+
+        for point in self {
+            guard let firstPoint = point.scaledPoints.first else { return path.cgPath }
+
+            path.move(to: firstPoint)
+
+            path.move(to: firstPoint)
+            for pointIndex in 1..<point.scaledPoints.count {
+                path.addLine(to: point.scaledPoints[pointIndex])
+            }
+        }
+
+        let mirror = CGAffineTransform(scaleX: 1,
+                                       y: -1)
+        let translate = CGAffineTransform(translationX: 0,
+                                          y: self.first!.imageSize.height)
+        var concatenated = mirror.concatenating(translate)
+
+        if let cgPath = path.cgPath.copy(using: &concatenated) {
+            return cgPath
+        } else {
+            return path.cgPath
+        }
+    }
+}
--- ImageEraser/Model/PointsSegment.swift
+++ ImageEraser/Model/PointsSegment.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import CoreGraphics
-import SwiftUI
 
 struct PointsSegment: Equatable {
     var rectPoints: [CGPoint]
@@ -15,33 +14,4 @@ struct PointsSegment: Equatable {
 
     var imageSize: CGSize
     var rectSize: CGSize
-}
-
-extension Array where Element == PointsSegment {
-    func scaledSegmentsToPath() -> CGPath {
-        var path = Path()
-
-        for point in self {
-            guard let firstPoint = point.scaledPoints.first else { return path.cgPath }
-
-            path.move(to: firstPoint)
-
-            path.move(to: firstPoint)
-            for pointIndex in 1..<point.scaledPoints.count {
-                path.addLine(to: point.scaledPoints[pointIndex])
-            }
-        }
-
-        let mirror = CGAffineTransform(scaleX: 1,
-                                       y: -1)
-        let translate = CGAffineTransform(translationX: 0,
-                                          y: self.first!.imageSize.height)
-        var concatenated = mirror.concatenating(translate)
-
-        if let cgPath = path.cgPath.copy(using: &concatenated) {
-            return cgPath
-        } else {
-            return path.cgPath
-        }
-    }
 }
