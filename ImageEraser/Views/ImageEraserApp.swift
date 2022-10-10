0324d36898bf89f4d8343629c342fac1706a275a
Christopher White <christopher.white@slicelife.com>
2022-10-10 20:38:34 +0100

    Removed Firebase

--- ImageEraser/Views/ImageEraserApp.swift
+++ ImageEraser/Views/ImageEraserApp.swift
@@ -6,21 +6,9 @@
 //
 
 import SwiftUI
-import FirebaseCore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
-
-        return true
-    }
-}
 
 @main
 struct ImageEraserApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
     var body: some Scene {
         WindowGroup {
             ContentView()
