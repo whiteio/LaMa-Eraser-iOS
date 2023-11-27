0df1aaa0043bee424256e7512c2c9c73368ce790
whiteio <chris@cwhite.io>
2023-11-27 22:18:27 +0000

    Remove initial background view

--- ImageEraser/Views/Landing/ContentView.swift
+++ ImageEraser/Views/Landing/ContentView.swift
@@ -11,38 +11,33 @@ import SwiftUI
 
 struct ContentView: View {
     @Bindable var navigationStore: NavigationStore
-    @State var hasAppeared = false
     @Namespace var namespace
 
     var body: some View {
-        if hasAppeared {
-            NavigationStack(path: $navigationStore.paths) {
-                VStack {
-                    ActionView()
+        NavigationStack(path: $navigationStore.paths) {
+            VStack {
+                ActionView()
+                    .environment(navigationStore)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(MainScreenBackgroundView())
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x: 200, y: 100))
+            .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .editPhoto(let photoData):
+                    let extractedExpr = EditViewModel()
+                    EditView(photoData: photoData)
+                        .environment(extractedExpr)
                         .environment(navigationStore)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(MainScreenBackgroundView())
-                .background(
-                    Image("Spline")
-                        .blur(radius: 50)
-                        .offset(x: 200, y: 100))
-                .navigationDestination(for: Route.self) { route in
-                    switch route {
-                    case .editPhoto(let photoData):
-                        let extractedExpr = EditViewModel()
-                        EditView(photoData: photoData)
-                            .environment(extractedExpr)
-                            .environment(navigationStore)
-                    }
-                }
-                .background(
-                    Color("MainBackgroundColor")
-                        .matchedGeometryEffect(id: "background", in: namespace))
-                .ignoresSafeArea()
             }
-        } else {
-            SplashscreenPlaceholderView(namespace: namespace, hasAppeared: $hasAppeared)
+            .background(
+                Color("MainBackgroundColor")
+                    .matchedGeometryEffect(id: "background", in: namespace))
+            .ignoresSafeArea()
         }
     }
 }
